d0853c21e690affbd383424ee372fec16c3bb667
tiffer <tiffer642@gmail.com>
2025-06-22 21:44:25 -0700

    Spawning Groundwork
    Added the ground work for the fishing hole spawning

---
+++ Assets/Scripts/FishingScripts/SpawnManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] spawnPoints;
+    public GameObject[] fishingHoles;
+
+    
+}
